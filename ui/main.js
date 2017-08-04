6946dbf9f861a86bd9bfcd7e6324e8fcc838ffb3
jagpretsingh504 <30655028+jagpretsingh504@users.noreply.github.com>
2017-08-04 12:52:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,16 @@
 console.log('Loaded!');
+
+var image = doceument.getElementById('madi');
+
+var marginLeft = 0;
+function moveRight(){
+    marginLeft +=1;
+    img.style.marginLeft = merginLeft + 'px';
+}
+
+
+
+
+img.onclick = function(){
+    var interval = setInterval(moveRight, 50);
+};
