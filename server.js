c4fdf5a9543e1732dccdc1b39fb0b0dd62c1ded8
jagpretsingh504 <30655028+jagpretsingh504@users.noreply.github.com>
2017-08-30 18:23:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get('/counter' , function(req, res){
 
 var pool = new Pool(config);
 
-app.get('test-db' , function(req, res)
+app.get('/test-db' , function(req, res)
                       {
     pool.query('SELECT * FROM test', function (err, result){
         if (err) {
