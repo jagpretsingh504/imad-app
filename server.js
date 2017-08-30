37eee9a98908198b102c5ecc71f787dbd70e7985
jagpretsingh504 <30655028+jagpretsingh504@users.noreply.github.com>
2017-08-30 18:19:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -31,9 +31,9 @@ app.get('test-db' , function(req, res)
     pool.query('SELECT * FROM test', function (err, result){
         if (err) {
             res.status(500).send(err.toString());
-        } else
+        } else{
         res.send(JSON.stringify(result));
-        
+        }
     });
 });
 
